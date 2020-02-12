960d348de9328907267a99fa2fce440e1ff91c5b
mnallamalli97 <krish@magnabid.com>
2020-02-11 16:19:40 -0800

    bootstrap installed to help with designs

--- frontend/js/components/HelloWorld/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelloWorld = () => {
-  return (
-    <p>Hello, world!</p>
-  );
-}
-
-export default HelloWorld;
---
+++ frontend/js/components/MainScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'reactstrap';
+
+const MainScreen = () => {
+  return (
+  	<React.Fragment>
+	    <p> Hello, world bitches! </p>
+	    <Button color="danger">Danger!</Button>
+	</React.Fragment>
+  );
+}
+
+export default MainScreen;
