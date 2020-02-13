389846e4c9fcf2166e8b0ebbff8b7555eea6fc22
mnallamalli97 <krish@magnabid.com>
2020-02-12 18:10:22 -0800

    sucessfully imported bootstrap. finished making UX responsive. finished first main screen (template screen)

--- frontend/js/components/MainScreen/index.js
+++ frontend/js/components/MainScreen/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import '../css/MainScreen.css';
 import { Button } from 'reactstrap';
+
 
 const MainScreen = () => {
   return (
-  	<React.Fragment>
-	    <p> Hello, world bitches! </p>
-	    <Button color="danger">testing....</Button>
-	</React.Fragment>
+  	<div className="MainScreen">
+    	<div className="buttons">
+	    	<Button className="button" color="success">Create Party
+	    		<small> You are the host </small>
+	    	</Button>
+		    <Button className="button" color="danger">Join Party
+		    	<small> Are you near a host? </small>
+		    </Button>
+  		</div>
+  	</div>
+
   );
 }
 
