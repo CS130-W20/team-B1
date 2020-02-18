6fd9edb6f83d8e19d276b1210c573580bd3fbc8c
mnallamalli97 <krish@magnabid.com>
2020-02-17 19:21:25 -0800

    party joined screen done

--- frontend/js/components/PartyJoined/index.js
+++ frontend/js/components/PartyJoined/index.js
@@ -14,8 +14,7 @@ const PartyJoined = () => {
 	            <FormControl
 	                type="text"
 	                placeholder="Search"
-	                className="mr-sm-2"
-	            />
+	                className="mr-sm-2"/>
 	        </Form>          
 		</div>
 
@@ -23,14 +22,34 @@ const PartyJoined = () => {
 			<h1>Song Queue </h1>
 		</div>
 
+
+
+
 	    <div className="listViewSongQueue">
 			<div class="list-group">
-				<a href="#" class="list-group-item list-group-item-action col-xs-9 listQueue">
-					fsdfsff
+				<a href="#" class="list-group-item list-group-item-action listQueue">
+					<div class="column left">
+						<img class="albumArtwork" src="https://www.w3schools.com/images/w3schools_green.jpg" alt="W3Schools.com" />
+					</div>
+
+					<div class="column middle">Name of Song</div>
+
+					<div class="column right buttonCard">
+						<Button className="SkipButton" color="danger">Skip</Button>
+					</div>
 				</a>
 
 			</div>
 	  	</div>
+
+
+
+
+
+
+
+
+
   	</div>
 
   );
