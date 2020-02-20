bbe0292588b4dfa26b5e83cf4f158f8b1b5cf6df
Benji Brandt <benjibrandt@ucla.edu>
2020-02-19 20:13:31 -0800

    [frontend/backend] removes outdated profile_manager. Adds rudimentary routing to the UI. (#23)

--- frontend/js/containers/App/index.js
+++ frontend/js/containers/App/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import { history } from '../../utils/history';
+
 import MainScreen from '../../components/MainScreen';
+import SpotifyLogin from '../../components/CreateParty/SpotifyLogin';
+import CreatePartyName from '../../components/CreateParty/CreatePartyName';
+import JoinPartyName from '../../components/JoinParty/JoinPartyName';
+import JoinPartyList from '../../components/JoinParty/JoinPartyList';
+import PartyJoined from '../../components/PartyJoined';
 
 const App = () => (
-	/* just for testing current screen UI. reaplce with MainScreen to make index.js the entry point */
-	<MainScreen /> 
+	<Router history={history}>
+		<Switch>
+			<Route exact path="/" component={MainScreen} />
+			<Route exact path="/host" component={CreatePartyName} />
+			<Route exact path="/login" component={SpotifyLogin} />
+			<Route exact path="/name" component={JoinPartyName} />
+			<Route exact path="/join" component={JoinPartyList} />
+			<Route exact path="/party" component={PartyJoined} /> {/* TODO: need to make URL tied to paty id/name */}
+		</Switch>
+	</Router>
 );
 
 export default App;
