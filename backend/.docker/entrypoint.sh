caea63dff55f113557aeef0d9409d89b77af1420
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2020-02-19 20:46:56 -0800

    Data Models: Song, User, SongRequest (#22)
    * docker: add the postgres database back in for our models
    
    * backend: introduce data models for User, Song, and SongRequest

--- backend/.docker/entrypoint.sh
+++ backend/.docker/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-python manage.py makemigrations
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started!"
+fi
+
+python manage.py makemigrations director
+python manage.py flush --no-input
 python manage.py migrate
 
 exec "$@"
